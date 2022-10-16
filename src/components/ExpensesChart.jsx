1d9d8908e6db60573db52f9440361da295ecc1a5
Marcelo Javier Aguila <marceloag@Mac-mini-de-Marcelo.local>
2022-10-16 08:25:43 -0300

    Removed auto from chart.js

--- src/components/ExpensesChart.jsx
+++ src/components/ExpensesChart.jsx
@@ -1,5 +1,21 @@
-import Chart from 'chart.js/auto/auto.esm';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+} from 'chart.js';
 import { Bar } from 'react-chartjs-2';
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 export const options = {
   responsive: true,
